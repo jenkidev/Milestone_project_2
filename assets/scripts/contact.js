395fb227892e0c1e5b2f8e5ea1afa23e3c087b39
jenkidev <jenkimc@googlemail.com>
2023-06-05 19:23:27 +0000

    Cleaned up contact.js with reccomendations from validation service

--- assets/scripts/contact.js
+++ assets/scripts/contact.js
@@ -1,6 +1,6 @@
 const form = document.getElementById("contactForm");
 
-function sendMail() {
+function sendMail(contactForm) {
     emailjs.send("service_zm8fwun", "template_1b3ozjh",
     {"from_name": contactForm.name.value,
      "from_email": contactForm.email.value,
@@ -9,12 +9,12 @@ function sendMail() {
     "Iw8TFysbugKKsDqz7")
     .then(function(response) {
         console.log('SUCCESS!', response.status, response.text);
-        alert("Your message has been sent")
+        alert("Your message has been sent");
      }, function(error) {
         console.log('FAILED...', error);
      });
      setTimeout(function() {
       form.reset();
     }, 3000);
-     return false
+     return false;
 }
